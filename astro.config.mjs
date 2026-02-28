0678d705e22778cdb1ec93f89b8ab28c9c160704
Zynith0 <nolan.lessard.music@gmail.com>
2026-02-27 23:54:42 -0500

    fix(about me): image rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://zynith0.github.io/blog',
-	base: '/blog',
+	base: '/zynith',
 	integrations: [mdx(), sitemap()],
 });
